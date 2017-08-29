ba4f3860b0e54cd1b5a7234babbcb1e5165bcd66
Rafael Makaha <lord.leafar.27@gmail.com>
2017-08-29 18:07:42 -0300

    Concertado erro de compilação

--- aula07/main.cpp
+++ aula07/main.cpp
@@ -6,7 +6,8 @@ int main(){
   Pessoa *p1 = new Pessoa();
   Aluno *a1 = new Aluno();
 
-  
+  cout <<"Este é o nome de p1: " + p1->getNome() << endl;
+  cout << "Este é o nome de a1: " + a1->getNome() <<endl;
 
   return 0;
 }
--- aula07/pessoa.cpp
+++ aula07/pessoa.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 	}
 
-	Pessoa::Pessoa(){}
-
 	string Pessoa::getNome(){
 		return nome;
 	}
