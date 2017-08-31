3132d3c8a39d381daf85375063936d76ebc0a3ab
Rafael Makaha <lord.leafar.27@gmail.com>
2017-08-31 09:13:36 -0300

    Herança testada e confirmada

--- aula07/main.cpp
+++ aula07/main.cpp
@@ -5,8 +5,9 @@ int main(){
 
   Pessoa *p1 = new Pessoa();
   Aluno *a1 = new Aluno();
-
-  cout <<"Este é o nome de p1: " + p1->getNome() << endl;
+  
+  a1->imprimirDados(); // a1 consegue acessar os métodos
+                      // herdados da Classe Pessoa
   cout << "Este é o nome de a1: " + a1->getNome() <<endl;
 
   return 0;
